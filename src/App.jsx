999ea0316beee52a929fa4418c160300f534b5ad
dariomrk <dariomrkonjic1@gmail.com>
2023-03-16 17:04:08 +0100

    Scaffold Board component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Cell from './components/Cell';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <>
+      <Cell content="cell" />
+      <Button content="button" />
+      <Button content="button" type="success" />
+      <Button content="button" type="danger" />
     </>
   );
 }
---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Board.scoped.css';
+
+function Board() {
+  return (
+  );
+}
+
+export default Board;
