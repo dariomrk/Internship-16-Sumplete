a08d556717561ace5449ac49f64b3e181ef65f51
dariomrk <dariomrkonjic1@gmail.com>
2023-03-21 09:33:24 +0100

    Adjust css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './App.scoped.css';
 import Game from './components/Game';
 
 function App() {
   return (
-    <Game />
+    <div className="app">
+      <div className="about">
+        <h1>Sumplete</h1>
+        <p>Delete numbers so each row/column adds up to the target number at the right/bottom.</p>
+      </div>
+      <Game />
+    </div>
   );
 }
 
